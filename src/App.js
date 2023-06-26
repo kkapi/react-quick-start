43fc65131e367be4d5ca41d2e20bf7d47524f2af
kkapi <hZG2fik_j8@mail.ru>
2023-06-26 20:46:39 +0300

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import MyButton from "./components/MyButton";
+import './styles/App.css'
 
 function App() {
   return (
     <div>
-      <h1>Welcome to my app</h1>
+      <h1 className="red">Welcome to my app</h1>
       <MyButton />
     </div> 
   );
