e09703a6e04de5b5022113c4df87c7fee2e7bdca
kkapi <hZG2fik_j8@mail.ru>
2023-06-26 21:31:37 +0300

    displaying data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import MyButton from "./components/MyButton";
 import './styles/App.css'
 
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
 function App() {
   return (
     <div>
       <h1 className="red">Welcome to my app</h1>
       <MyButton />
+      <h2>{user.name}</h2>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"Photo" + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
     </div> 
   );
 }
