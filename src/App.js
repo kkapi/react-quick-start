d1e322dda1dd88202203e6d503d43094d879a046
kkapi <hZG2fik_j8@mail.ru>
2023-06-26 20:40:16 +0300

    creating and nesting components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import MyButton from "./components/MyButton";
 
 function App() {
   return (
     <div>
-      test
-    </div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div> 
   );
 }
 
